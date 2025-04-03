ea844b29f728b1d5c0b6a09c324c49679a437d5e
Ashish Mishra <ash.ishu025@gmail.com>
2025-04-03 21:40:12 +0530

    rest

---
+++ ES6/Rest.js
@@ -0,0 +1,16 @@
+//REST - Representational State Transfer - Consventions (Guidelines) Protocol
+
+//HTTP/HTTPS - HyperText Transfer Protocol
+
+//HTTP/s - is statless protocol so to save the information retrived/manipulated by this protocol we need to explicitely save
+// this information to DB via Services, if it follow REST convention we say it restful
+
+//Methods/Verbs used
+//Get - to fetch
+//Post - to create
+//Update/Put - to udpate
+//Patch - to update minimum info
+//Delete - to delete info via id
+
+
+//HTML - HyperText Markup Language , node structure like XML
