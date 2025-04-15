37e6a4d403b32a74350d6e3bf5404227e0ca31f3
Ashish Mishra <ash.ishu025@gmail.com>
2025-04-15 21:29:10 +0530

    webpack setup

--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -7,7 +7,7 @@ import * as ReactDOM from "react-dom/client"
 //creating root of the react application where we can load the react app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//hooking or injecting module to our index html page
+//bootstrapping the react application
 root.render(
     <ApplicationComponent/>
 )
