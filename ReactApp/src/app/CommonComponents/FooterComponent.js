f101de0165099a9b9d303ededb081b3d4fda657a
Ashish Mishra <ash.ishu025@gmail.com>
2025-04-17 21:34:43 +0530

    footer component

---
+++ ReactApp/src/app/CommonComponents/FooterComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+let Footer = (props)=>{
+    
+    return(
+        <div>
+            <h1>This is application footer {props.sessionName}</h1>
+        </div>
+    )
+}
+
+export default Footer;
+
+
+//Virtual Dom
+
+//DOM
--- ReactApp/src/app/application.js
+++ ReactApp/src/app/application.js
@@ -1,18 +1,35 @@
 import React, {Component} from "react";
+import Footer from "./CommonComponents/FooterComponent";
 
 export default class ApplicationComponent extends Component {
 
+    /**
+     *
+     */
+    constructor(props) {
+        super();
+        this.state = {
+            userName : "SomeUser",
+            userAddress : "SomeAddress"
+        }
+    }
+
     render(){
-        let name = "Joel", x=5, y = 9
+        let name = "Joel", x=5, y = 9;
+        let sessionName = "MERNStack - React Props"
         return( //vitual dom or jsx code (javascript like xml structure)
             <div>
                 <h1>This is coming from Application Component</h1>
                 
                 <b>{name}</b>
                 <hr />
-                <b>Multiply 5*9 = {5*9} </b>
+                <b>Multiply 5*10 = {5*10} </b>
                 <hr />
                 <b>Sum 5+9 = {5+9} </b>
+                <hr />
+                <b>{this.state.userName}</b>
+                <hr />
+                <Footer sessionName={sessionName}/>
             </div>            
         )
     }
