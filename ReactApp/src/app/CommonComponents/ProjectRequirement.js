6b1e9cd5c65cab4f63b015ae2250b0758bf53a07
Ashish Mishra <ash.ishu025@gmail.com>
2025-05-23 21:39:30 +0530

    cart implementation

--- ReactApp/src/app/CommonComponents/ProjectRequirement.js
+++ ReactApp/src/app/CommonComponents/ProjectRequirement.js
@@ -8,3 +8,13 @@
 // Create an action method to add the detail to database using a server api
 // Server Side - Create product router and api to save the product using productdatamodel
 // Everything should be done in continuation with shopping cart project
+
+
+// 27-May-2025 - Cart Implementation
+
+// Cart Implementation
+// Create New Cart Component using react hooks, functional component
+// Each Item in this component should be added from Product Component 
+// Now each Product should have general fields, like Name, Description, Rating, Price, Category (New Product Document/ Collection)
+// In Product component each item when we click to display details should also have a button "Add To Item" on click should add to New Cart
+// On Cart Component, Button for save to Checkout should save the cart item to database (New Cart Document/ Collection)
