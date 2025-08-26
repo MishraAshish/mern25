cb0e73e8ee056cfac31c7c05c290cd3bd528b5fc
Ashish Mishra <ash.ishu025@gmail.com>
2025-08-26 20:34:43 +0530

    angular setup and running code

---
+++ TypeScript/intallation_Angular.js
@@ -0,0 +1,13 @@
+// install angular cli globally
+// npm i -g @angular/cli
+
+// create a new angular project
+// ng new AngularApp
+
+// select the settings as per your requirement
+
+// navigate to the project directory
+// cd AngularApp
+
+// serve the application
+// ng serve --open
