cb12306b9f5bd33fe9dff78f16ea8d42b8986ec1
Ashish Mishra <ash.ishu025@gmail.com>
2025-09-08 20:31:41 +0530

    app routing

--- AngularApp/src/app/app.routes.ts
+++ AngularApp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./components/home/home').then(m => m.Home) },
+    { path: 'about', loadComponent: () => import('./components/about/about').then(m => m.About) }
+];
--- AngularApp/src/app/app.ts
+++ AngularApp/src/app/app.ts
@@ -1,19 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    Home],
+    RouterOutlet, 
+    Home, 
+    About, 
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
+
 export class App {
   protected readonly title = signal('AngularApp');
 
   inputCounter1 = 0;
-
   numberDataOutput = 0;
 }
